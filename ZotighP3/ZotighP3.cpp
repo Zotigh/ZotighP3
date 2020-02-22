615896b9bc650659f0ca8bfdf620965a99a5f6a2
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-22 12:41:01 -0700

    Add project files.

---
+++ ZotighP3/ZotighP3.cpp
@@ -0,0 +1,13 @@
+// ZotighP3.cpp
+// Lance Zotigh (lzotigh1@cnm.edu)
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Lance Zotigh" << endl;
+	cout << "Pourpose: a program to calculate the surface area of segments of the five planets that you can see from the earth with the unaided eye and of the moon" << endl;
+	cout << "Calculating a Lune" << endl;
+}
