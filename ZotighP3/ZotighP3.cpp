962144fcd7c061502b3d7c1b5490f5bd5042a757
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-22 12:41:50 -0700

    Inital Commit

--- ZotighP3/ZotighP3.cpp
+++ ZotighP3/ZotighP3.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+	// Header
 	cout << "Lance Zotigh" << endl;
 	cout << "Pourpose: a program to calculate the surface area of segments of the five planets that you can see from the earth with the unaided eye and of the moon" << endl;
 	cout << "Calculating a Lune" << endl;
