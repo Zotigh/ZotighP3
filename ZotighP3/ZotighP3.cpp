08d53cfceb5c1ad62caf258c7f256b1623f15d45
Lance Zotigh <lancezotighlz@gmail.com>
2020-02-22 13:39:11 -0700

    Added some logic

--- ZotighP3/ZotighP3.cpp
+++ ZotighP3/ZotighP3.cpp
@@ -2,6 +2,8 @@
 // Lance Zotigh (lzotigh1@cnm.edu)
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,6 +13,7 @@ const double pi = 3.14159;
 int main()
 {
 	//variables
+	vector<string> namesOfPlanets;
 	string usersName;
 	string usersChoice;
 	int planetChoice;
@@ -39,6 +42,7 @@ int main()
 		cin >> planetChoice;
 		cin.ignore();
 
+		// Checks to make sure the user is with in the correct threshold
 		while(planetChoice <= 0 || planetChoice >= 7)
 		{
 			cout << "Selection not within the range of options";
@@ -47,18 +51,25 @@ int main()
 			cin.ignore();
 		} 
 
-		cout << "What is the percentage of the face that is illuminated: ";
+		// Asks the user for the percent
+		cout << "What is the percentage of the face that is illuminated (ex 100% = 100): ";
 		cin >> percentageIlluminated;
 		cin.ignore();
 		
-		while (percentageIlluminated >= 0 && percentageIlluminated <= 100) 
+		// Checks to make sure the user is with in the correct threshold
+		while (percentageIlluminated <= 0 || percentageIlluminated >= 101) 
 		{
 			cout << "number entered is not in range" << endl;
-			cout << "Do you want to calculate another question.”.   "
+			cout << "Do you want to calculate another question";
 			cin >> percentageIlluminated;
 			cin.ignore();
 		}
 		
+		// Makes percent illuminated a decimal
+		percentageIlluminated /= 100;
+
+		cout << percentageIlluminated;
+
 
 	} while (0);
 
